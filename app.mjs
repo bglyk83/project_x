249760e2e9a4733f14c0c35ce66217b7065c029e
Yunki <bglyk83@gmail.com>
2025-04-24 14:50:01 +0900

    배열을 이용한 API 테스트

--- app.mjs
+++ app.mjs
@@ -3,7 +3,7 @@ import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
 
 const app = express();
-app.use(express());
+app.use(express.json());
 
 app.use("/posts", postsRouter);
 app.use("/auth", authRouter);
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,28 +1,35 @@
 import express from "express";
+import * as postController from "../controller/post.mjs";
+
 const router = express.Router();
 
 // 모든 포스트 가져오기
-// 해당 ID에 대한 포스트 가져오기
-// GET 방식
-// http://127.0.0.1:8080/posts/ - 모든 포스트
-// http://127.0.0.1:8080/posts?userid=?? - 해당 사용자 포스트 가져오기
+// 해당 아이디에 대한 포스트 가져오기
+// GET
+// http://127.0.0.1:8080/posts/
+// http://127.0.0.1:8080/posts?userid=apple
+router.get("/", postController.getPosts);
 
 // 글번호에 대한 포스트 가져오기
 // GET
 // http://127.0.0.1:8080/posts/:id
+router.get("/:id", postController.getPost);
 
 // 포스트 쓰기
-// Post
-// http://127.0.0.1:8080/posts/
+// POST
+// http://127.0.0.1:8080/posts
 // json 형태로 입력 후 저장
+router.post("/", postController.createPost);
 
-// 포스트 수정
-// put
+// 포스트 수정하기
+// PUT
 // http://127.0.0.1:8080/posts/:id
 // json 형태로 입력 후 저장
+router.put("/:id", postController.updatePost);
 
-// 포스트 삭제
-// delete
+// 포스트 삭제하기
+// DELETE
 // http://127.0.0.1:8080/posts/:id
+router.delete("/:id", postController.deletePost);
 
 export default router;
